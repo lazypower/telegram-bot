6a5801a0ba381673c1d123e40fa5d025a6d3c28a
Charles Butler <chuck@dasroot.net>
2015-04-18 22:03:14 -0400

    Update the rps plugin with a helpful chart command

--- plugins/rockpaperscissors.lua
+++ plugins/rockpaperscissors.lua
@@ -1,14 +1,25 @@
 do
 
-function run(msg, matches)
-  local answers = {'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'}
-  return answers[math.random(#answers)]
+local function run(msg, matches)
+  if matches[1] == "!rpshelp" then
+    local chart_url = "http://a.tgcdn.net/images/products/additional/large/db01_lizard_spock.jpg"
+    send_document_from_url(receiver, chart_url)
+  else
+    local answers = {'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'}
+    return answers[math.random(#answers)]
+  end
 end
 
 return {
   description = "Settle Arguments",
-  usage = "!rps",
-  patterns = {"^!rps"},
+  usage = {
+           "!rps: Play rock paper scissors lizard spock",
+           "!rpschart: Forgot the rules? look at the chart"
+           },
+  patterns = {
+              "^!rps",
+              "^!rpshelp"
+             },
   run = run
 }
 
