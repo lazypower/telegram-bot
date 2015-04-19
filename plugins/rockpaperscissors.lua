e852f930dd6c07570da902da5c7578574970e8c2
Charles Butler <chuck@dasroot.net>
2015-04-18 22:30:05 -0400

    Another attempt at adding the chart, with a simpler method body

--- plugins/rockpaperscissors.lua
+++ plugins/rockpaperscissors.lua
@@ -1,20 +1,9 @@
 do
 
-local function prep_chart(cb_extra, success, result)
-    local receiver = cb_extra.receiver
-    local chart_url = cb_extra.gif_url
-    send_document_from_url(receiver, chart_url)
-end
-
 local function run(msg, matches)
   if matches[1] == "!chart" then
-    local text = "Have a chart, everybody love charts"
     local chart_url = "http://a.tgcdn.net/images/products/additional/large/db01_lizard_spock.jpg"
-    local cb_extra = {
-        chart_url = chart_url,
-        receiver = receiver
-    }
-    send_msg(receiver, text, prep_chart, cb_extra)
+    send_photo_from_url(receiver, chart_url)
   else
     local answers = {'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'}
     return answers[math.random(#answers)]
