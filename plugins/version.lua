d0ca84fbee1c09d3f65f6a166166f1db148a576b
Charles Butler <chuck@dasroot.net>
2015-04-18 21:47:45 -0400

    Repoint the version identifier to our octobot repository

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,18 +1,18 @@
 do
 
 function run(msg, matches)
-  return 'Telegram Bot '.. VERSION .. [[ 
-  Checkout http://git.io/6jdjGg
+  return 'Telegram Bot '.. VERSION .. [[
+  Checkout http://git.io/vvNg4
   GNU GPL v2 license.]]
 end
 
 return {
-  description = "Shows bot version", 
+  description = "Shows bot version",
   usage = "!version: Shows bot version",
   patterns = {
     "^!version$"
-  }, 
-  run = run 
+  },
+  run = run
 }
 
 end
