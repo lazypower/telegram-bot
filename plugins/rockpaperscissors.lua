e23bd763e0085f8768b517c7ffdcaec3b6d15107
Charles Butler <chuck@dasroot.net>
2015-04-18 22:15:58 -0400

    Callback method to prep data, and ultimately send

--- plugins/rockpaperscissors.lua
+++ plugins/rockpaperscissors.lua
@@ -1,9 +1,20 @@
 do
 
+local function prep_chart(cb_extra, success, result)
+    local receiver = cb_extra.receiver
+    local chart_url = cb_extra.gif_url
+    send_document_from_url(receiver, chart_url)
+end
+
 local function run(msg, matches)
   if matches[1] == "!chart" then
+    local text = "Have a chart, everybody love charts"
     local chart_url = "http://a.tgcdn.net/images/products/additional/large/db01_lizard_spock.jpg"
-    send_document_from_url(receiver, chart_url)
+    local cb_extra = {
+        chart_url = chart_url,
+        receiver = receiver
+    }
+    send_msg(receiver, text, prep_chart, cb_extra)
   else
     local answers = {'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'}
     return answers[math.random(#answers)]
