eb74b2af418bce1eae7385e11dbd7398d238c29a
Anthony Satkowski <asatkowski@universityboundagency.com>
2015-05-01 13:30:29 -0400

    basic start for pokemon plugin

--- plugins/testing.lua
+++ plugins/testing.lua
@@ -1,12 +1,25 @@
-local function run(msg, matches)
-  return matches[1]
-end
+do
+	api_root = "http://pokeapi.co/"
+	api_pokedex = "api/pokedex/1/"
 
-return {
-  description = "This description is a shitpost until further notice xD :P :^)",
-  usage = "!echo_test [whatever]: echoes the msg",
-  patterns = {
-    "^!echo_test (.*)$"
-  }, 
-  run = run 
-}
+	local function run(msg, matches)
+	  local pokemon = matches[1];
+
+	  if full_pokedex == nil then
+	  	local pokedex_request = httpRequest(api_root..api_pokedex)
+	  	full_pokedex = pokedex_request.pokemon
+	  end
+
+	  return full_pokedex[0].name
+	end
+
+	return {
+	  description = "This description is a shitpost until further notice xD :P :^)",
+	  usage = "!echo_test [whatever]: echoes the msg",
+	  patterns = {
+	    "^!ichooseyou (.*)$"
+	  }, 
+	  run = run 
+	}
+
+end
