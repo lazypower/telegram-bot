efca3bf6af49bd522cf33fb25cb4617c949c0732
Charles Butler <chuck@dasroot.net>
2015-05-03 21:36:58 -0400

    Force a git pull from the build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+cd /home/ubuntu/dockerfile/telegram-bot
+git pull origin master
+
 /home/ubuntu/hooked/build-hook.py -r ~/dockerfile/telegram-bot -t octobot -s ~/dockerfile/telegram-bot/runme.sh -v &
